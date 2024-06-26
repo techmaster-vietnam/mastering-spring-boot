1a9b29e9ff5f7528b4341209948a0d2f7af491ab
Dung Ta Van <itprono3@gmail.com>
2024-06-26 15:03:15 +0700

    spring boot

---
+++ spring-boot-hello-world/src/main/java/vn/techmaster/hello_world/HelloWorldStartUp.java
@@ -0,0 +1,10 @@
+package vn.techmaster.hello_world;
+
+import org.springframework.boot.SpringApplication;
+
+public class HelloWorldStartUp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldStartUp.class);
+    }
+}
