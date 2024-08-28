488fe272237d88678000d8f8aa322770eca0a40e
Dung Ta Van <itprono3@gmail.com>
2024-08-28 10:43:44 +0700

    jpa

--- spring-boot-jpa/src/main/java/vn/techmaster/jpa/SpringBootJpaStartUp.java
+++ spring-boot-jpa/src/main/java/vn/techmaster/jpa/SpringBootJpaStartUp.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.domain.PageRequest;
 import vn.techmaster.jpa.entity.Person;
 import vn.techmaster.jpa.repo.PersonRepository;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @EnableCaching
@@ -26,5 +29,36 @@ public class SpringBootJpaStartUp {
             person.getId()
         );
         System.out.println("fetchedPersonById: " + fetchedPersonById);
+
+        List<Person> fetchedPersonsByNameIn = personRepository
+            .findByNameIn(Arrays.asList("Techmaster", "hello"));
+        System.out.println("fetchedPersonsByNameIn: " + fetchedPersonsByNameIn);
+
+        Person fetchedPersonByName = personRepository
+            .findFirstByName("hello");
+        System.out.println("fetchedPersonByName: " + fetchedPersonByName);
+
+        Person fetchedPersonByNameNot = personRepository
+            .findFirstByNameNot("hello");
+        System.out.println("fetchedPersonByNameNot: " + fetchedPersonByNameNot);
+
+        List<Person> fetchedPersonsByNameLikeJpql = personRepository
+            .findByNameLikeJpql(
+                "t"
+            );
+        System.out.println("fetchedPersonsByNameLikeJpql: " + fetchedPersonsByNameLikeJpql);
+
+        List<Person> fetchedPersonsByNameLikeNative = personRepository
+            .findByNameLikeNative(
+                "t"
+            );
+        System.out.println("fetchedPersonsByNameLikeNative: " + fetchedPersonsByNameLikeNative);
+
+        List<Person> fetchedPersonsPagination = personRepository
+            .findByNameLikeJpqlPagination(
+                "t",
+                PageRequest.of(0, 10)
+            );
+        System.out.println("fetchedPersonsPagination: " + fetchedPersonsPagination);
     }
 }
--- spring-boot-jpa/src/main/java/vn/techmaster/jpa/repo/PersonRepository.java
+++ spring-boot-jpa/src/main/java/vn/techmaster/jpa/repo/PersonRepository.java
@@ -1,7 +1,8 @@
 package vn.techmaster.jpa.repo;
 
-
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import vn.techmaster.jpa.entity.Person;
 
@@ -9,4 +10,30 @@ import java.util.Collection;
 import java.util.List;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {}
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    // lấy bản ghi đầu tiên của trường name bằng tham số truyền vào
+    Person findFirstByName(String name);
+
+    // lấy bản ghi đầu tiên có trường name khác tham số truyền vào
+    Person findFirstByNameNot(String name);
+
+    // lấy tất cả các bản ghi có trường name nằm trong danh sách của tham số truyền vào
+    List<Person> findByNameIn(Collection<String> names);
+
+    // Truy vấn danh sách các bản ghi có trường name gần đúng với tham số truyền vào
+    @Query("SELECT e FROM Person e WHERE e.name LIKE %?1%")
+    List<Person> findByNameLikeJpql(String keyword);
+
+    @Query(
+        value = "SELECT * FROM persons WHERE name LIKE %?1%",
+        nativeQuery = true
+    )
+    List<Person> findByNameLikeNative(String keyword);
+
+    @Query("SELECT e FROM Person e WHERE e.name LIKE %?1%")
+    List<Person> findByNameLikeJpqlPagination(
+        String keyword,
+        Pageable pageable
+    );
+}
